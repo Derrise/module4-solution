b794a1732b1dea1a42435a944fc6927b1db0d23b
Derrise <93504856+Derrise@users.noreply.github.com>
2025-10-25 00:36:49 +0500

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,10 +1,9 @@
-(function (window) {
-  var byeSpeaker = {};
-  var speakWord = "Good Bye";
+(function(window) {
+	var speakWord = "Good Bye";
+	var byeSpeaker = function (name) {
+  		console.log(speakWord + " " + name);
+	}
+	
+	window.byeSpeaker = byeSpeaker;
 
-  byeSpeaker.speak = function (name) {
-    console.log(speakWord + " " + name);
-  };
-
-  window.byeSpeaker = byeSpeaker;
 })(window);
